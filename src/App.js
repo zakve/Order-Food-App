6c45eade422b46b52ce3dd9bae01b0481f4f7867
zakve <zaklas.martin@gmail.com>
2021-09-23 15:44:36 +0200

    Remove unused code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { Container } from '@mui/material';
-import './App.css';
 import Header from './components/Layout/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-        <Header />
-      </Container>
-    </div>
+    <Container>
+      <Header />
+    </Container>
   );
 }
 
