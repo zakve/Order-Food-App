28ef8736d0deaa2fefb4976e96fe32cb4de9d492
zakve <zaklas.martin@gmail.com>
2021-09-30 09:33:13 +0200

    Add Meals component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Container } from '@mui/material';
 import Header from './components/Layout/Header';
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <Container>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Container>
   );
 }
