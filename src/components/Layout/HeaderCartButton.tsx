8a0250b61c559b98a72eb50d78396778fd538fab
zakve <zaklas.martin@gmail.com>
2021-10-07 21:11:23 +0200

    Add Dialog to CartButton

--- src/components/Layout/HeaderCartButton.tsx
+++ src/components/Layout/HeaderCartButton.tsx
@@ -1,17 +1,54 @@
 import React from 'react'
-import { Button } from '@mui/material'
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, List, ListItem, ListItemIcon, ListItemText, IconButton } from '@mui/material'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-
 interface HeaderCartButtonProps {
     price: string;
     currency: string;
 }
 
 const HeaderCartButton = ({ price = "0", currency = "€" }: HeaderCartButtonProps) => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
-        <Button variant="outlined" startIcon={<ShoppingCartIcon />}>
-            {price} {currency}
-        </Button>
+        <>
+            <Button
+                onClick={handleOpen}
+                variant="outlined"
+                startIcon={<ShoppingCartIcon />}
+            >
+                {price} {currency}
+            </Button>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Your Cart"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        {/* <List>
+                            <ListItem>
+                                <ListItemText id='dialogText' primary={`sushi`} />
+                                <IconButton edge="end" aria-label="comments">
+                                    <ShoppingCartIcon />
+                                </IconButton>
+                            </ListItem>
+                        </List> */}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Close</Button>
+                    <Button onClick={handleClose} autoFocus>
+                        Order
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
     )
 }
 
